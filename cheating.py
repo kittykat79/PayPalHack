f84d339941e6ce5982f5c9c60abbb9cc5b8c8c78
DarshanMurthy <darshants.darshan1@gmail.com>
2016-07-09 22:17:45 -0700

    ob

--- cheating.py
+++ cheating.py
@@ -1,32 +1,28 @@
 from yelpauthdata import Yelp
 
 class cheating(object):
-	def __init__(self, action, target):
-		self.action=action
+	def __init__(self, target):
 		self.target=target
 
-	def actions(self):
-		if(self.action=="fix" and self.target=="doorknob"):
-			yelp=Yelp()
-			client=yelp.clientAuth()
-			return client
+	def targets(self):
+		yelp=Yelp()
+		client=yelp.clientAuth()
+		params = {'term': self.target, 'lang': 'en','deals_filter':'true'}
+		response=client.search('San Jose', **params)
+		print(response.businesses)
+		for i in range(3):
+			print(response.businesses[i].name)
+		return client
 
 
 
-obj = cheating("fix","doorknob")
-client=obj.actions()
+obj = cheating("doorknob")
+client=obj.targets()
 
-params = {
-    'term': 'doorknob',
-    'lang': 'en'
-}
 
-response=client.search('San Francisco', **params)
 
 #response = client.search('San Francisco')
-print(response.businesses)
-for i in range(3):
-	print(response.businesses[i].name)
+
 
 
 
